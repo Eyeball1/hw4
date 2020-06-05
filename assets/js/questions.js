7af852a63ad278cdaeea1555a0143de5be5a3fde
Eyeball1 <61758955+Eyeball1@users.noreply.github.com>
2020-06-05 16:19:12 -0700

    Create questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,35 @@
+// list of all questions, choices, and answers
+var questions = [
+  {
+    title: "Which of the following is not a datatype?",
+    choices: ["strings", "booleans", "Readme.md", "numbers"],
+    answer: "Readme.md"
+  },
+  {
+    title: "The condition in an else statement is enclosed within:",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    title: "Arrays in JavaScript can be used to store ____.",
+    choices: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above"
+    ],
+    answer: "all of the above"
+  },
+  {
+    title:
+      "String values must be enclosed within ____ when being assigned to variables.",
+    choices: ["commas", "curly brackets", "quotes", "parentheses"],
+    answer: "quotes"
+  },
+  {
+    title:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+    answer: "console.log"
+  }
+];
